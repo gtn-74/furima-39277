b5fd731aaac1831d5fea0030dd2163d87a0f050c
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-21 22:10:14 +0900

    カテゴリ選択機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
+
   def index
+    #@items = Item.order("category_id DESC")
   end
 
   def new
+    @item = Item.new
   end
-  
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :sale_id, :shopping_id, :prefecture_id, :shopping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
-  
   belongs_to :user
-
-  validates :name, presence: true
-  validates :content, presence: true
-  validates :category_id, presence: true
-  validates :sale_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shopping_id, presence: true
-  validates :price, presence: true
-
+  has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sale
+  belongs_to :shopping
+  belongs_to :prefecture
+  belongs_to :shopping_day
+
+ 
+  validates :name, :content, presence: true
+  validates :category_id, :sale_id, :shopping_id, :prefecture_id, :shopping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+            format: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
+
+
 end
