22f6c1e89a9800de148be83421bc15a3e79c41ab
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-22 14:56:18 +0900

    販売額の非同期通信実装
    js実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,16 @@
-function item (){
-  console.log("非同期投稿を実装");
+function Price(){
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  itemPrice.addEventListener('input', function() {
+    const Price = itemPrice.value;
+
+    const Tax = Math.floor(Price * 0.1);
+    addTaxPrice.textContent = Tax;
+
+    const ProfitValue = Price - Tax;
+    profit.textContent = ProfitValue;
+  });
 };
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', Price);
