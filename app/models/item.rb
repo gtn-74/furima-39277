6a7db2c3eb9716a3cdd8711af07ffd541f8e94f6
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-23 13:09:17 +0900

    02商品出品機能
    コードレビュー2回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
   validates :name, :content, :image, :price, presence: true
   validates :category_id, :sale_id, :shopping_id, :prefecture_id, :shopping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                     format: { with: /\A[0-9]+\z/, message: ' is not a number' }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -60,6 +60,21 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include 'Price is not a number'
         end
+        it '販売価格が300円未満では出品できない' do
+          @item.price = 299
+          @item.valid?
+          expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
+        end
+        it '販売価格が10,000,000円を超過すると出品できない' do
+          @item.price = 10_000_000
+          @item.valid?
+          expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
+        end
+        it 'userが紐づいていない場合登録できない' do
+          @item.user_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include
+        end
       end
     end
   end
