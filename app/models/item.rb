7d181fd3cab855f2cb889d21e717a35da4a335ab
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-23 14:40:46 +0900

    04商品出品機能
    コードレビュー4回目
    format: { with: /\A[0-9]+\z/, message: ' is not a number' }の削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,5 @@ class Item < ApplicationRecord
   validates :name, :content, :image, :price, presence: true
   validates :category_id, :sale_id, :shopping_id, :prefecture_id, :shopping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/, message: ' is not a number' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
