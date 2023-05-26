1e003b847dc97f76479a2d9088789e4a7d2c4c80
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-26 18:40:38 +0900

    該当商品情報を購入ページで表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,24 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def new
+    @order_address = OrderAddress.new
   end
 
   def create
-    #@order = Order.create(order_params)
-    #Address.create(address_params)
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+      else
+        render :index
+      end
   end
 
   private
-  #def order_params
-    #params.require(:order).permit(:item_id).merge(user_id: current_user.id)
-  
-
-  #def address_params
-    #params.require(:address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(order_id: @order.id)
-  
+  def order_params
+    params.require(:order_address).permit(:price, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
