4f75569af68c93a01ed24d3a1d16ed1ae597d197
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-31 11:37:18 +0900

    validate修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,12 +6,11 @@ class OrderAddress
   with_options presence: true do
     validates :city
     validates :address
-    validates :phone_number
     validates :user_id
     validates :item_id
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
   end
   
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe OrderAddress, type: :model do
       end
 
       it 'prefectureを選択していないと保存できないこと' do
-        @order_address.prefecture_id = 0
+        @order_address.prefecture_id = 1
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
       end
