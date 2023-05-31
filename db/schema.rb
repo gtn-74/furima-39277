f633df728563e4156e9b6a23253cfb16222941e7
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-31 11:56:42 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2023_05_25_084954) do
     t.string "city", null: false
     t.string "address", null: false
     t.string "building_name"
-    t.string "phone_number"
+    t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
