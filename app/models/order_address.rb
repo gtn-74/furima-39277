376460cdc84db19f5aca035dc8f5be6e4e21f148
ogitaniryosuke <gtn.r74@gmail.com>
2023-06-01 16:41:46 +0900

    エラーコードを日本語表記に
    追加実装
    order_address（購入機能）の日本語可

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,9 +9,9 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :token
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は無効です。ハイフン(-)を含む"}
+    validates :prefecture_id, numericality: {other_than: 1 }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
   
   def save
