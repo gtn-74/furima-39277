e4a72665d20df5e9dae168c7ebd3ec04655dc6be
ogitaniryosuke <gtn.r74@gmail.com>
2023-06-02 23:08:58 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.253.47.208 IP', user: 'ec2-user', roles: %w{app db web}
+server '54.253.47.208', user: 'ec2-user', roles: %w{app db web}
