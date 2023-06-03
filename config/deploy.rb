e90915ee05f936d49404d5207d3f76bb680a737c
ogitaniryosuke <gtn.r74@gmail.com>
2023-06-03 10:59:05 +0900

    deploy修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.3'
 set :application, 'furima-39277'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/ogitaniryosuke/furima-39277.git'
+set :repo_url,  'git@github.com:ogitaniryosuke/furima-39277.git'
 set :branch, "main"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
